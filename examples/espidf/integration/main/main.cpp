662d2a07f2961fb3692875170192745fe61a4713
Johan Böhlin <johboh@gmail.com>
2025-05-05 19:23:20 +0200

    Remove crypt, we should now use johboh/GCMEncyption instead

--- examples/espidf/integration/main/main.cpp
+++ examples/espidf/integration/main/main.cpp
@@ -1,20 +1,9 @@
-#include <EspNowCrypt.h>
+#include <esp-now-structs.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-
-// Encyption key used for our own packet encryption (GCM).
-// We are not using the esp-now encryption due to the peer limitation.
-// The key should be the same for both the host and the node.
-const char esp_now_encryption_key[] = "0123456789ABCDEF"; // Must be exact 16 bytes long. \0 does not count.
-
-// Used to validate the integrity of the messages.
-// The secret should be the same for both the host and the node.
-const char esp_now_encryption_secret[] = "01234567"; // Must be exact 8 bytes long. \0 does not count.
-
-EspNowCrypt _esp_now_crypt(esp_now_encryption_key, esp_now_encryption_secret);
 
 extern "C" {
 void app_main();
 }
 
-void app_main(void) {}
+void app_main(void) { EspNowMessageHeaderV1 headerv1; }
--- src/EspNowNetworkShared.h
+++ src/EspNowNetworkShared.h
@@ -1,7 +1,6 @@
 #ifndef __ESP_NOW_NETWORK_SHARED_H__
 #define __ESP_NOW_NETWORK_SHARED_H__
 
-#include <EspNowCrypt.h>
 #include <esp-now-structs.h>
 
 #endif // __ESP_NOW_NETWORK_SHARED_H__
